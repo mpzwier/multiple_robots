4cc9b9131cf58b22a5180240c9e4401660a0fcac
Zwier <123642966+mpzwier@users.noreply.github.com>
2026-04-09 12:00:12 +0200

    Add files via upload

--- multiple_robots/bringup/robots.launch.py
+++ multiple_robots/bringup/robots.launch.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -10,74 +11,101 @@ from ros_gz_bridge.actions import RosGzBridge
 from ros_gz_sim.actions import GzServer
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.event_handlers import OnProcessExit
+from nav2_common.launch import RewrittenYaml
 
 
 
 def generate_launch_description():
     # Package paths
-    pkg_share = get_package_share_directory('multiple_robots')
-    ros_gz_sim_share = get_package_share_directory('ros_gz_sim')
-    nav2_share = get_package_share_directory('nav2_bringup')
+    pkg_share=get_package_share_directory('multiple_robots')
+    ros_gz_sim_share=get_package_share_directory('ros_gz_sim')
+    nav2_share=get_package_share_directory('nav2_bringup')
 
 
     # File paths
-    world_path = os.path.join(pkg_share, 'world', 'simple_world.sdf')
-    remappings = [('/tf', 'tf'), ('/tf_static', 'tf_static')]
+    world_path=os.path.join(pkg_share, 'world', 'simple_world.sdf')
+    remappings=[('/tf', 'tf'), ('/tf_static', 'tf_static')]
+    dock_locations_path=os.path.join(pkg_share, 'config', 'dock_locations.yaml')
+
+    # Load dock database from YAML
+    with open(dock_locations_path, 'r') as f:
+        dock_database_dict = yaml.safe_load(f)
 
     # Robots information
 
     robots = [
         {
             'name': 'robot1',
-            'x': '-7.5',
-            'y': '-8.5',
-            'yaw': '0.0',
+            'controller_type': 'swerve_drive_controller',
+            'x': '0.75',
+            'y': '0.75',
+            'yaw': '1.5708',
             'model': os.path.join(pkg_share, 'description', 'robot1_swerve_bot.urdf'),
             'controller': os.path.join(pkg_share, 'config', 'robot1_swerve_drive_controllers_params.yaml'),
             'nav2': os.path.join(pkg_share, 'config', 'robot1_nav2_params.yaml'),
             'ekf': os.path.join(pkg_share, 'config', 'robot1_ekf.yaml'),
             'bridge': os.path.join(pkg_share, 'config', 'robot1_bridge_config.yaml'),
             'rviz': os.path.join(pkg_share, 'rviz', 'robot1_config.rviz'), 
-            'map': os.path.join(pkg_share, 'config', 'robots_map.yaml')
+            'map': os.path.join(pkg_share, 'config', 'robots_map.yaml'),
+            'scan_mask': os.path.join(pkg_share, 'config', 'scan_mask.yaml')
         },
         {
             'name': 'robot2',
-            'x': '-5.5',
-            'y': '-8.5',
-            'yaw': '0.0',
+            'controller_type': 'swerve_drive_controller',
+            'x': '2.45',
+            'y': '0.75',
+            'yaw': '1.5708',
             'model': os.path.join(pkg_share, 'description', 'robot2_swerve_bot.urdf'),
             'controller': os.path.join(pkg_share, 'config', 'robot2_swerve_drive_controllers_params.yaml'),
             'nav2': os.path.join(pkg_share, 'config', 'robot2_nav2_params.yaml'),
             'ekf': os.path.join(pkg_share, 'config', 'robot2_ekf.yaml'),
             'bridge': os.path.join(pkg_share, 'config', 'robot2_bridge_config.yaml'),
             'rviz': os.path.join(pkg_share, 'rviz', 'robot2_config.rviz'), 
-            'map': os.path.join(pkg_share, 'config', 'robots_map.yaml')           
+            'map': os.path.join(pkg_share, 'config', 'robots_map.yaml'),   
+            'scan_mask': os.path.join(pkg_share, 'config', 'scan_mask.yaml')        
         },
         {
             'name': 'robot3',
-            'x': '-3.5',
-            'y': '-8.5',
-            'yaw': '0.0',
+            'controller_type': 'swerve_drive_controller',
+            'x': '3.90',
+            'y': '0.5',
+            'yaw': '1.5708',
             'model': os.path.join(pkg_share, 'description', 'robot3_swerve_bot.urdf'),
             'controller': os.path.join(pkg_share, 'config', 'robot3_swerve_drive_controllers_params.yaml'),
             'nav2': os.path.join(pkg_share, 'config', 'robot3_nav2_params.yaml'),
             'ekf': os.path.join(pkg_share, 'config', 'robot3_ekf.yaml'),
             'bridge': os.path.join(pkg_share, 'config', 'robot3_bridge_config.yaml'),
             'rviz': os.path.join(pkg_share, 'rviz', 'robot3_config.rviz'), 
-            'map': os.path.join(pkg_share, 'config', 'robots_map.yaml')           
+            'map': os.path.join(pkg_share, 'config', 'robots_map.yaml'),
+            'scan_mask': os.path.join(pkg_share, 'config', 'scan_mask.yaml')          
         }
+        # {
+        #     'name': 'robot4',
+        #     'controller_type': 'diff_drive_controller',
+        #     'x': '3.90',
+        #     'y': '0.5',
+        #     'yaw': '1.5708',
+        #     'model': os.path.join(pkg_share, 'description', 'robot4_diff_bot.urdf'),
+        #     'controller': os.path.join(pkg_share, 'config', 'robot4_diff_drive_controllers_params.yaml'),
+        #     'nav2': os.path.join(pkg_share, 'config', 'robot4_nav2_params.yaml'),
+        #     'ekf': os.path.join(pkg_share, 'config', 'robot4_ekf.yaml'),
+        #     'bridge': os.path.join(pkg_share, 'config', 'robot4_bridge_config.yaml'),
+        #     'rviz': os.path.join(pkg_share, 'rviz', 'robot4_config.rviz'), 
+        #     'map': os.path.join(pkg_share, 'config', 'robots_map.yaml'),
+        #     'scan_mask': os.path.join(pkg_share, 'config', 'scan_mask.yaml')          
+        # }
     ]
 
     # Gazebo
 
-    start_gazebo = IncludeLaunchDescription(
+    start_gazebo=IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(ros_gz_sim_share, 'launch', 'gz_sim.launch.py')),
         launch_arguments={
             'gz_args': f"-r {world_path}"
         }.items(),      
     )
 
-    clock = Node(
+    clock=Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         output='screen',
@@ -85,12 +113,24 @@ def generate_launch_description():
     )
 
     # Multi robot launch
-
+  
     robot_groups=[]
     for robot in robots:
 
         namespace=robot['name']
 
+        # RewrittenYaml merges it into the docking_server params
+        nav2_params = RewrittenYaml(
+        source_file=robot['nav2'],
+        param_rewrites={'docking_server.dock_database': dock_database_dict},
+        convert_types=True
+        )      
+
+        # Conditional remapping logic based on the controller type
+        remap_tf = []  # Default: no remapping for tf
+        if robot['controller_type'] == 'diff_drive_controller':  
+            remap_tf = [('/tf', f'/{namespace}/tf')]    
+       
         robot_group=GroupAction([
 
             # GZ-ROS bridge
@@ -124,13 +164,22 @@ def generate_launch_description():
                 executable='create',
                 output='screen',
                 arguments=[
-                    '-topic', f'{namespace}/robot_description', 
+                    '-topic', f'/{namespace}/robot_description', 
                     '-name', robot['name'],
                     '-x', robot['x'],
                     '-y', robot['y'],
                     '-Y', robot['yaw']],
             ),
-           
+
+            # Lidar scan filtered
+            Node(
+                package='scan_mask_filter',
+                executable='scan_mask_node',
+                namespace=namespace,
+                output='screen',
+                parameters=[robot['scan_mask']],
+            ),
+
             # Localization
             Node(
                 package='robot_localization',
@@ -157,18 +206,21 @@ def generate_launch_description():
                     f'/{namespace}/controller_manager',
                     ],
             ),
+            
+
             Node(
                 package='controller_manager',
                 executable='spawner',
                 output="screen",
                 namespace=namespace,
                 arguments=[
-                    'swerve_drive_controller',
+                    robot['controller_type'], 
                     '--controller-manager',
                     f'/{namespace}/controller_manager',
                     '--param-file',
                     robot['controller'],
                 ],
+                remappings=remap_tf,
             ),
 
             # Rivz
@@ -191,7 +243,7 @@ def generate_launch_description():
 
             # Nav2 delayed
             TimerAction(
-                period=2.0,  
+                period=2.0,
                 actions=[
                     IncludeLaunchDescription(
                         PythonLaunchDescriptionSource(
@@ -201,9 +253,10 @@ def generate_launch_description():
                             'use_sim_time': 'True',
                             'autostart': 'True',
                             'map': robot['map'],
-                            'params_file': robot['nav2'],
+                            'params_file': nav2_params, #robot['nav2'],
                             'namespace': namespace,
                             'use_namespace': 'True',
+                            'use_composition': 'True',
                         }.items(),
                     )
                 ]
@@ -212,6 +265,7 @@ def generate_launch_description():
          
         ])
         robot_groups.append(robot_group)
+   
 
 
   
